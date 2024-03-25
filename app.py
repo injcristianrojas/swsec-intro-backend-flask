5224117514eb9b130d821bc769a6bed88a9a55d8
Cristián Rojas <injcristianrojas@gmail.com>
2024-03-25 11:14:26 -0300

    Folder name change

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 from db import init_db
-from v1.api import api_v1
+from v2.api import api_v2
 
 app = Flask(__name__)
-app.register_blueprint(api_v1)
+app.register_blueprint(api_v2)
 
 if __name__ == "__main__":
     init_db()
--- v2/api.py
+++ v2/api.py
@@ -2,9 +2,9 @@ import sqlite3
 from flask import Blueprint, jsonify
 from db import connect_db
 
-api_v1 = Blueprint('simple_page', __name__)
+api_v2 = Blueprint('APIV1', __name__)
 
-@api_v1.route("/api/v2/messages")
+@api_v2.route("/api/v2/messages")
 def messages():
     conn = connect_db()
     cur = conn.cursor()
