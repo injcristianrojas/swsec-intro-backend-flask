14f1a35b95b42f1dbea4ff2e1f11d236d2ccd989
Cristián Rojas <injcristianrojas@gmail.com>
2024-04-19 16:07:36 -0400

    Starting JWT

--- v2/api.py
+++ v2/api.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
+import datetime
+from flask import Blueprint, jsonify, request
 from db import connect_db
+import jwt
 
-api = Blueprint('APIv2', __name__)
+api = Blueprint("APIv2", __name__)
+
+SECRET_KEY = "12345"
+ALGORITHM = "HS256"
 
 @api.route("/messages")
 def messages():
@@ -16,9 +21,10 @@ def messages():
 
     json_results = []
     for row in results:
-        json_results.append({'id': row[0], 'message': row[1]})
-    
+        json_results.append({"id": row[0], "message": row[1]})
+
     return jsonify(json_results)
+
 
 @api.route("/users/type/<id>")
 def get_users_by_type(id):
@@ -33,6 +39,21 @@ def get_users_by_type(id):
 
     json_results = []
     for row in results:
-        json_results.append({'id': row[0], 'username': row[1]})
-    
+        json_results.append({"id": row[0], "username": row[1]})
+
     return jsonify(json_results)
+
+
+@api.route("/auth", methods = ['POST'])
+def authenticate():
+    data = request.form
+    username = data.get("username")
+    password = data.get("password")
+    if True:
+        payload = {
+            "username": username,
+            "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+        return jsonify({"token": token})
+    return jsonify({"message": "Invalid credentials"}), 401
