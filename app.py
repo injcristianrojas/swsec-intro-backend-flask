2c87da2d57bcd4f73f642eb8522c0e860b0aa99d
Cristián Rojas <injcristianrojas@gmail.com>
2024-04-28 16:39:53 -0400

    CORS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from v1.api import api as api_v1
 from v2.api import api as api_v2
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'])
+CORS(app, origins=['*'])
 
 app.register_blueprint(api_v1, url_prefix='/api/v1')
 app.register_blueprint(api_v2, url_prefix='/api/v2')
