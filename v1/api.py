08d1638544c7e20bfb24a5b5eecec0a63ff3dba1
Cristián Rojas <injcristianrojas@gmail.com>
2024-04-29 17:43:37 -0400

    V1: Insert messages

--- v1/api.py
+++ v1/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from db import connect_db
 
 api = Blueprint('APIv1', __name__)
@@ -7,11 +7,8 @@ api = Blueprint('APIv1', __name__)
 def messages():
     conn = connect_db()
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM messages")
-
     results = cur.fetchall()
-
     conn.close()
 
     json_results = []
@@ -20,15 +17,24 @@ def messages():
     
     return jsonify(json_results)
 
+@api.route("/messages/add", methods = ['POST'])
+def post_message():
+    message = request.get_json().get('message')
+    print(message)
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')");
+    conn.commit()
+    conn.close()
+
+    return jsonify([{'status': 'OK'}])
+
 @api.route("/users/type/<id>")
 def get_users_by_type(id):
     conn = connect_db()
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM users WHERE user_type = " + id)
-
     results = cur.fetchall()
-
     conn.close()
 
     json_results = []
