8e457a04d5ec25902af9fd3dae45cbb0f8b65a77
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-04 12:57:42 -0400

    Swagger is now on different versions

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
 
-from flask_restx import Api, Resource
+from flask_restx import Api
 
-from db import init_db, connect_db
-from v1.api import api as api_v1
-from v2.api import api as api_v2
+from db import init_db
+from v1.api import blueprint as api_v1
+#from v2.api import api as api_v2
 
 app = Flask(__name__)
 CORS(app, origins=["*"])
@@ -14,58 +14,11 @@ api = Api(
     app,
     version="1.0",
     title="My API",
-    description="A simple Flask API",
-    doc="/swaggerui",
+    description="A simple Flask API"
 )
 
 app.register_blueprint(api_v1, url_prefix="/api/v1")
-app.register_blueprint(api_v2, url_prefix="/api/v2")
-
-
-@api.route("/messages")
-class Messages(Resource):
-    def get(self):
-        conn = connect_db()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM messages")
-        results = cur.fetchall()
-        conn.close()
-
-        json_results = []
-        for row in results:
-            json_results.append({"id": row[0], "message": row[1]})
-
-        return jsonify(json_results)
-
-
-@api.route("/messages/add")
-class AddMessage(Resource):
-    def post(self):
-        message = request.get_json().get("message")
-        print(message)
-        conn = connect_db()
-        cur = conn.cursor()
-        cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')")
-        conn.commit()
-        conn.close()
-
-        return jsonify([{"status": "OK"}])
-
-
-@api.route("/users/type/<id>")
-class UsersByType(Resource):
-    def get(self, id):
-        conn = connect_db()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE user_type = " + id)
-        results = cur.fetchall()
-        conn.close()
-
-        json_results = []
-        for row in results:
-            json_results.append({"id": row[0], "username": row[1], "password": row[2]})
-
-        return jsonify(json_results)
+#app.register_blueprint(api_v2, url_prefix="/api/v2")
 
 
 if __name__ == "__main__":
--- v1/api.py
+++ v1/api.py
@@ -1,43 +1,54 @@
 from flask import Blueprint, jsonify, request
+from flask_restx import Api, Resource
 from db import connect_db
 
-api = Blueprint('APIv1', __name__)
+blueprint = Blueprint('APIv1', __name__)
+api = Api(blueprint, version="1.0",
+    title="My API v1",
+    description="A simple Flask API",
+    doc="/swaggerui",)
 
 @api.route("/messages")
-def messages():
-    conn = connect_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages")
-    results = cur.fetchall()
-    conn.close()
+class Messages(Resource):
+    def get(self):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages")
+        results = cur.fetchall()
+        conn.close()
 
-    json_results = []
-    for row in results:
-        json_results.append({'id': row[0], 'message': row[1]})
-    
-    return jsonify(json_results)
+        json_results = []
+        for row in results:
+            json_results.append({"id": row[0], "message": row[1]})
 
-@api.route("/messages/add", methods = ['POST'])
-def post_message():
-    message = request.get_json().get('message')
-    conn = connect_db()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')");
-    conn.commit()
-    conn.close()
+        return jsonify(json_results)
 
-    return jsonify([{'status': 'OK'}])
+
+@api.route("/messages/add")
+class AddMessage(Resource):
+    def post(self):
+        message = request.get_json().get("message")
+        print(message)
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')")
+        conn.commit()
+        conn.close()
+
+        return jsonify([{"status": "OK"}])
+
 
 @api.route("/users/type/<id>")
-def get_users_by_type(id):
-    conn = connect_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE user_type = " + id)
-    results = cur.fetchall()
-    conn.close()
+class UsersByType(Resource):
+    def get(self, id):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE user_type = " + id)
+        results = cur.fetchall()
+        conn.close()
 
-    json_results = []
-    for row in results:
-        json_results.append({'id': row[0], 'username': row[1], 'password': row[2]})
-    
-    return jsonify(json_results)
+        json_results = []
+        for row in results:
+            json_results.append({"id": row[0], "username": row[1], "password": row[2]})
+
+        return jsonify(json_results)
