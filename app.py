8ab692bb29bbbc3b9563f1bb109fc101c542578f
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-04 12:05:00 -0400

    Swagger working on v1

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_cors import CORS
 
-from db import init_db
+from flask_restx import Api, Resource, fields
+
+from db import init_db, connect_db
 from v1.api import api as api_v1
 from v2.api import api as api_v2
 
 app = Flask(__name__)
-CORS(app, origins=['*'])
+CORS(app, origins=["*"])
 
-app.register_blueprint(api_v1, url_prefix='/api/v1')
-app.register_blueprint(api_v2, url_prefix='/api/v2')
+api = Api(app, version="1.0", title="My API", description="A simple Flask API", doc='/swaggerui')
+
+app.register_blueprint(api_v1, url_prefix="/api/v1")
+app.register_blueprint(api_v2, url_prefix="/api/v2")
+
+@api.route("/messages")
+class Messages(Resource):
+    def get(self):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM messages")
+        results = cur.fetchall()
+        conn.close()
+
+        json_results = []
+        for row in results:
+            json_results.append({'id': row[0], 'message': row[1]})
+        
+        return jsonify(json_results)
+
+@api.route("/users/type/<id>")
+class UsersByType(Resource):
+    def get(self, id):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE user_type = " + id)
+        results = cur.fetchall()
+        conn.close()
+
+        json_results = []
+        for row in results:
+            json_results.append({'id': row[0], 'username': row[1], 'password': row[2]})
+        
+        return jsonify(json_results)
 
 if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0', port=9000)
+    app.run(debug=True, host="0.0.0.0", port=9000)
