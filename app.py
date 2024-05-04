0636eb31fe043ebff1b8d08fdc1b42c0999b1782
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-04 12:17:26 -0400

    Works OK on V1 (self-contained)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from flask_restx import Api, Resource, fields
@@ -10,10 +10,17 @@ from v2.api import api as api_v2
 app = Flask(__name__)
 CORS(app, origins=["*"])
 
-api = Api(app, version="1.0", title="My API", description="A simple Flask API", doc='/swaggerui')
+api = Api(
+    app,
+    version="1.0",
+    title="My API",
+    description="A simple Flask API",
+    doc="/swaggerui",
+)
 
 app.register_blueprint(api_v1, url_prefix="/api/v1")
 app.register_blueprint(api_v2, url_prefix="/api/v2")
+
 
 @api.route("/messages")
 class Messages(Resource):
@@ -26,9 +33,24 @@ class Messages(Resource):
 
         json_results = []
         for row in results:
-            json_results.append({'id': row[0], 'message': row[1]})
-        
+            json_results.append({"id": row[0], "message": row[1]})
+
         return jsonify(json_results)
+
+
+@api.route("/messages/add")
+class AddMessage(Resource):
+    def post(self):
+        message = request.get_json().get("message")
+        print(message)
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO messages(message) VALUES ('" + message + "')")
+        conn.commit()
+        conn.close()
+
+        return jsonify([{"status": "OK"}])
+
 
 @api.route("/users/type/<id>")
 class UsersByType(Resource):
@@ -41,9 +63,10 @@ class UsersByType(Resource):
 
         json_results = []
         for row in results:
-            json_results.append({'id': row[0], 'username': row[1], 'password': row[2]})
-        
+            json_results.append({"id": row[0], "username": row[1], "password": row[2]})
+
         return jsonify(json_results)
+
 
 if __name__ == "__main__":
     init_db()
