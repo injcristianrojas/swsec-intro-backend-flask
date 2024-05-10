64f687da00dd7b2a5750880215a24e600668d0a4
Cristián Rojas <injcristianrojas@gmail.com>
2024-05-09 21:15:49 -0400

    Added new user

--- db.py
+++ db.py
@@ -19,6 +19,7 @@ def init_db():
     cur.execute("INSERT INTO users(username,password,user_type) VALUES ('admin', '123', 1)");
     cur.execute("INSERT INTO users(username,password,user_type) VALUES ('zorzal', 'fio', 2)");
     cur.execute("INSERT INTO users(username,password,user_type) VALUES ('chincol', 'fiofio', 2)");
+    cur.execute("INSERT INTO users(username,password,user_type) VALUES ('tiuque', 'pah', 2)");
 
     conn.commit()
     conn.close()
